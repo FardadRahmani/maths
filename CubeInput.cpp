55c176f062363f01a4615116b48ceb8adb7fad19
FardadRahmani <rahmanifardad@yahoo.de>
2024-05-05 15:42:11 +0200

    Create CubeInput.cpp
    Asks user for a real number and computes the cube.

---
+++ CubeInput.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+//liest 2 Zeilen ein
+using namespace std;
+int main()
+{
+	float n1 = 0.0f;
+	
+	cout << "Enter a real number: ";
+	cin >> n1;
+	
+	
+	cout << n1 << " ^ 3 = " << pow(n1,3) << endl;
+	
+	return 0;
+}
+
